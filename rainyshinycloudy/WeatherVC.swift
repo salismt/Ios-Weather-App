a0da0d6448982ed0f9092f88dd177190c8603a7d
Salis <m.salis@go-jek.com>
2016-12-20 21:30:10 +0700

    setup layout for table view and IBoutlets

--- rainyshinycloudy/WeatherVC.swift
+++ rainyshinycloudy/WeatherVC.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
