45ee560d8b27a9aed83523b03c46cadc150f0ed4
Salis <m.salis@go-jek.com>
2016-12-21 09:35:35 +0700

    add api constants

---
+++ rainyshinycloudy/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  rainyshinycloudy
+//
+//  Created by Salis on 12/21/16.
+//  Copyright © 2016 Salis. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "2a443bede129d761da9a02d270a3a4c7"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
---
+++ rainyshinycloudy/CurrentWeather.swift
@@ -0,0 +1,58 @@
+//
+//  CurrentWeather.swift
+//  rainyshinycloudy
+//
+//  Created by Salis on 12/21/16.
+//  Copyright © 2016 Salis. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class CurrentWeather {
+    
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    var _highTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+    
+        return _date
+    }
+    
+    var currentTemp: String {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+    
+    
+    
+}
