e5eb4e4886ace8a5fba705e499a2110ed613cff3
Salis <m.salis@go-jek.com>
2016-12-21 22:08:00 +0700

    parse data

--- rainyshinycloudy/CurrentWeather.swift
+++ rainyshinycloudy/CurrentWeather.swift
@@ -60,8 +60,33 @@ class CurrentWeather {
         Alamofire.request(currentWeatherURL!).responseJSON { response in
             let result = response.result
             print(response)
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
+                    if let currentTemperature = main["temp"] as? Double {
+                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5) - 459.67)
+                        let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision / 10))
+                        
+                        self._currentTemp = kelvinToFarenheit
+                        print("value \(self._currentTemp) and \(kelvinToFarenheit)")
+                    }
+                }
+            }
         }
         completed()
     }
-    
+
 }
